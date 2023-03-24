5c2c1077e594188a1f54fcabafcc73686f972e21
练建发 <958823465@qq.com>
2023-03-24 21:01:15 +0800

    路由守卫

--- src/vue-router/index.js
+++ src/vue-router/index.js
@@ -6,6 +6,77 @@ import {createWebHashHistory} from "./history/hash";
 import {createWebHistory} from "./history/html5";
 import { createRouterMatcher } from "./matcher";
 
+function useCallback(){
+  const handlers = []
+  function add(handler){
+    handlers.push(handler);
+  }
+  return {
+    add,
+    list:()=>handlers
+  }
+}
+
+function extractChangeRecords(to,from){
+  const leavingRecords = [];
+  const updatingRecords  = [];
+  const enteringRecords = [];
+  const len = Math.max(to.matched.length,from.matched.length);
+
+  for(let i = 0;i < len;i++){
+    const recordFrom = from.matched[i]
+    if(recordFrom){
+      if(to.matched.find(record => record.path === recordFrom.path)){
+        // 去和来一样，更新
+        updatingRecords.push(recordFrom);
+      }else{
+        // 不一样就是 离开
+        leavingRecords.push(recordFrom);
+      }
+    }
+
+    const recordTo = to.matched[i]
+    if(recordTo){
+      if(!from.matched.find(record => record.path === recordTo.path)){
+        // 去的有，来的没有，进入
+        enteringRecords.push(recordTo);
+      }
+    }
+  }
+
+  return [
+    leavingRecords,
+    updatingRecords,
+    enteringRecords,
+  ]
+}
+
+function guardToPromise(guard,to,from,record){
+  return ()=>new Promise((resolve,reject)=>{
+    const next = ()=>resolve();
+    const guardReturn = guard.call(record,to,from,next);
+    //如果没有调用next就帮他调用
+    return Promise.resolve(guardReturn).then(next);
+  })
+}
+
+function extractComponentsGuards(matched,guardType,to,from){
+  const guards = [];
+  for(const record of matched){
+    const rawComponent = record.components.default;
+    const guard = rawComponent[guardType];
+    // 将所有的路由守卫串联在一起
+    guard && guards.push(guardToPromise(guard,to,from,record))
+  }
+  return guards;
+}
+
+//组合守卫调用链
+function runGuardQueue(guards){
+  return guards.reduce((promise,guard)=>{
+    return promise.then(()=>guard());
+  },Promise.resolve())
+}
 
 // 初始化系统的默认参数
 const START_LOCATION_NORMALIZED = {
@@ -29,6 +100,10 @@ function createRouter(options){
     reactiveRoute[key] = computed(()=>currentRoute.value[key])
   }
   // 如果之后不想通过 currentRoute.value 来访问数据可以再用 obj= reactive(currentRoute) 包裹一下
+
+  const beforeGuards = useCallback()
+  const afterGuards = useCallback();
+  const beforeResolveGuards = useCallback();
 
   function resolve(to){ // 可以是 to="/" :to={path:"/"}
     if(typeof to === "string"){
@@ -65,13 +140,70 @@ function createRouter(options){
     console.log(currentRoute.value,routerHistory.state);
   }
 
+  async function navigate(to,from){
+    //需要知道是从 哪 到 哪的，谁更新
+    const [leavingRecords,updatingRecords,enteringRecords] = extractChangeRecords(to,from);
+
+    //抽离组件守卫钩子
+    let guards = extractComponentsGuards(
+      leavingRecords.reverse(),
+      "beforeRouteLeave",
+      to,
+      from,  
+    );
+    
+    return runGuardQueue(guards).then(()=>{
+      guards = [];
+      for(const guard of beforeGuards.list()){
+        guards.push(guardToPromise(guard,to,from,guard))
+      }
+      return runGuardQueue(guards);
+    }).then(()=>{
+      guards = extractComponentsGuards(
+        updatingRecords,
+        "beforeRouteUpdate",
+        to,
+        from,  
+      );
+      return runGuardQueue(guards);
+    }).then(()=>{
+      guards = [];
+      for(const record of to.matched){
+        if(record.beforeEnter){
+          guards.push(guardToPromise(record.beforeEnter,to,from,record))
+        }
+      }
+      return runGuardQueue(guards);
+    }).then(()=>{
+      guards = extractComponentsGuards(
+        enteringRecords,
+        "beforeRouteEnter",
+        to,
+        from,  
+      );
+      return runGuardQueue(guards);
+    }).then(()=>{
+      guards = [];
+      for(const guard of beforeResolveGuards.list()){
+        guards.push(guardToPromise(guard,to,from,guard))
+      }
+      return runGuardQueue(guards);
+    })
+  }
+
   //通过路径匹配到对应的记录，更新currentRoute
   function pushWithRedirect(to){
     const targetLocation = resolve(to);
     const from = currentRoute.value
 
-    //根据是不是第一次来决定是 push 还是 replace
-    finalizeNavigation(targetLocation,from)
+    //路由守卫处理
+    navigate(targetLocation,from).then(()=>{
+      //根据是不是第一次来决定是 push 还是 replace
+      finalizeNavigation(targetLocation,from)
+    }).then(()=>{
+      // 导航结束，调用 afterEach
+      for(const guard of afterGuards.list()) guard(to,from);
+    })
   }
 
   function push(to,data){
@@ -80,9 +212,9 @@ function createRouter(options){
 
   const router = {
     push,
-    beforeRouter,
-    afterRouter,
-    beforeResolve,
+    beforeEach:beforeGuards.add,
+    afterEach:afterGuards.add,
+    beforeResolve:beforeResolveGuards.add,
     install(app){ //路由的核心就是监听路由变化更新页面
       const router = this;
       app.config.globalProperties.$router = router;
